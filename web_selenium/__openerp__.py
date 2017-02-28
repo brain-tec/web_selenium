dc924f003c2176eb2de4430e7f4e4c7cdc7c270c
Alejandro Sanchez <alejandro.sanchez@braintec-group.com>
2017-02-28 09:00:49 +0100

    openerp

--- web_selenium/__openerp__.py
+++ web_selenium/__openerp__.py
@@ -8,7 +8,7 @@
     "version": "9.0.1.0.0",
     "category": "Hidden",
     "website": "http://www.brain-tec.ch",
-    "author": "Andreas Stauder <andreas.stauder@brain-tec.ch>",
+    "author": "Andreas Stauder <andreas.stauder@brain-tec.ch> and Alejandro Sanchez <alejandro.sanchez@braintec-group.com>",
     "depends": ["web"],
     "qweb": [
         'static/src/xml/base_ext.xml',
