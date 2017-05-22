8fe4c984a55b8de204b90f22de908b27fbc823d4
Alejandro Sanchez-Paton <BT-asanchez@users.noreply.github.com>
2017-05-22 11:56:23 +0200

    Update __openerp__.py

--- web_selenium/__openerp__.py
+++ web_selenium/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "We Selenium",
+    "name": "Web Selenium",
     "summary": "Record your testcases with Selenium",
     "version": "9.0.1.0.0",
     "category": "Hidden",
